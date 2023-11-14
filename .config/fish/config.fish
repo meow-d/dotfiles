0bcac9840b2b6af79323c0825a194ac0e3908205
meow-d <isaacwenyi@gmail.com>
2023-11-14 15:39:50 +0800

    Migrate fish abbr config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,3 +23,21 @@ end
 ### Init
 zoxide init fish | source
 starship init fish | source
+abbr -a -- f flatpak
+abbr -a -- fi 'flatpak install'
+abbr -a -- fs 'flatpak search'
+abbr -a -- fu 'flatpak uninstall'
+abbr -a -- n micro
+abbr -a -- nn sudoedit
+abbr -a -- p paru
+abbr -a -- pi 'paru -dS'
+abbr -a -- pu 'paru -dR'
+abbr -a -- r rpm-dostree
+abbr -a -- ri 'rpm-dostree install'
+abbr -a -- ru 'rpm-dostree uninstall'
+abbr -a -- s sudo
+abbr -a -- za 'zoxide add'
+abbr -a -- zq 'zoxide query'
+abbr -a -- zqi zoxide\ query\ \\x2di
+abbr -a -- zr 'zoxide remove'
+abbr -a -- di 'sudo dnf install -y'
